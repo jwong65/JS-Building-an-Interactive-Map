d0b0711b37e215818c536157f7003a96522cdd87
unknown <jason.wong6507@gmail.com>
2022-11-10 19:14:02 -0500

    Successfully added the redPin to marker

--- public/app.js
+++ public/app.js
@@ -37,24 +37,24 @@ var polygon = L.polygon([
 // create red pin marker
 
 var redPin = L.icon({
-    iconUrl: './red-pin.png',
+    iconUrl: './assets/red-pin.png',
 
     iconSize: [38,38],
     iconAnchor: [19,38]
 
 })
 
-const redpin= L.marker([], {icon: redPin}).bindPopup('Reaumur-Sebastopol')
+//const redpin= L.marker([48.866200610611926, 2.352236247419453], {icon: redPin}).addTo(myMap).bindPopup('Reaumur-Sebastopol')
 
 // Metro station markers:
-const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('Reaumur-Sebastopol')
-const sSD = L.marker([48.869531786321566, 2.3528590208055196]).bindPopup('Strasbourg-Saint-Denis')
+//const rS = L.marker([48.866200610611926, 2.352236247419453]).bindPopup('Reaumur-Sebastopol')
+const sSD = L.marker([48.869531786321566, 2.3528590208055196], {icon: redPin}).bindPopup('Strasbourg-Saint-Denis')
 const sentier = L.marker([48.8673721067762, 2.347107922912739]).bindPopup('Sentier')
 const bourse = L.marker([48.86868503971672, 2.3412285142058167]).bindPopup('Bourse')
 const qS = L.marker([48.869560129483226, 2.3358638645569543]).bindPopup('Quatre Septembre')
 const gB = L.marker([48.871282159004856, 2.3434818588892714]).bindPopup('Grands Boulevards')
  
-const stations = L.layerGroup([rS, sSD, sentier, bourse, qS, gB]).addTo(myMap)
+const stations = L.layerGroup([sSD, sentier, bourse, qS, gB]).addTo(myMap)
 
 
 
